db876866aa85820ca15057500f8944e887a5e8d1
Raphico <raphicogit@gmail.com>
2025-07-23 22:14:32 +0100

    feat: respond with body

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -29,34 +29,114 @@ func main() {
 	}
 
 	handleConnection(conn)
-
 }
 
 func handleConnection(conn net.Conn) {
+	var response Response = NewResponse(conn)
+
 	reader := bufio.NewReader(conn)
 	requestLine, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Failed to read request: ", err.Error())
+		response.Write(StatusInternalServerError, nil)
+		response.Send()
 		return;
 	}
 
 	parts := strings.Split(strings.TrimSpace(requestLine), " ")
 	if len(parts) < 3 {
-		writeResponse(conn, "400", "Bad Request")
+		response.Write(StatusBadRequest, nil)
+		response.Send()
 		return;
 	}
 
 	path := parts[1]
-	switch path {
-	case "/":
-		writeResponse(conn, "200", "OK")
+	switch {
+	case path == "/":
+		response.Write(StatusOk, nil)
+		response.Send()
+	case strings.HasPrefix(path, "/echo/"):
+		parts = strings.Split(path, "/")
+		if len(parts) != 3 {
+			response.Write(StatusBadRequest, nil)
+			response.Send()
+			return;
+		}
+
+		msg := parts[2];
+		response.Write(StatusOk, []byte(msg))
+		response.headers.Set("Content-Type", "text/plain")
+		response.headers.Set("Content-Length", "3")
+		response.Send()
+
 	default:
-		writeResponse(conn, "404", "Not Found")
+		response.Write(StatusNotFound, nil)
+		response.Send()
 	}
 }
 
+const (
+	StatusOk = 200
+	StatusBadRequest = 400
+	StatusNotFound = 404
+	StatusInternalServerError = 500
+)
 
-func writeResponse(conn net.Conn, statusCode string, reason string) {
-	response := fmt.Sprintf(`HTTP/1.1 %s %s\r\n\r\n`, statusCode, reason)
-	conn.Write([]byte(response))
+var StatusText = map[int]string {
+	StatusOk: "OK",
+	StatusBadRequest: "Bad Request",
+	StatusNotFound: "Not Found",
+	StatusInternalServerError: "Internal Server Error",
+}
+
+type Header map[string][]string
+
+type Response struct {
+	conn net.Conn
+	statusCode int
+	body []byte
+	headers  Header
+}
+
+func NewResponse(conn net.Conn) Response {
+	return Response {
+		conn: conn,
+		headers: make(Header),
+	}
+}
+
+func (r *Response) Header() Header {
+	return r.headers
+}
+
+func (h *Header) Set(key string, value string) {
+	(*h)[key] = append((*h)[key], value)
+}
+
+func (r *Response) Write(statusCode int, body []byte) {
+	r.body = body
+	r.statusCode = statusCode
+}
+
+func (r *Response) Send () {
+	var builder strings.Builder	
+	
+	// write status line
+	statusLine := fmt.Sprintf("HTTP/1.1 %d %s\r\n", r.statusCode, StatusText[r.statusCode])
+	builder.WriteString(statusLine)
+
+	// write http headers
+	for key, values := range r.headers {
+		for _, v := range values {
+			fmt.Fprintf(&builder, "%s: %s\r\n", key, v)
+		}
+	}
+
+	// end headers
+	builder.WriteString("\r\n")
+
+	// write body
+	builder.WriteString(string(r.body))
+
+	r.conn.Write([]byte(builder.String()))
 }
